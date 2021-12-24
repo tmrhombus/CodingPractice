8e5a583f35f534356ef2f4b9adb6f0089a626038
Tom Perry <tomperry7@gmail.com>
2021-12-24 16:32:34 -0500

    outfile csv for each iteration

--- kmeans.cpp
+++ kmeans.cpp
@@ -5,6 +5,7 @@
 #include <iostream>  // for stdio
 // #include <sstream>   // for file-reading
 #include <vector>
+#include <cmath>  // for abs
 
 struct Point {
     double x, y;     // coordinates
@@ -83,17 +84,12 @@ void associatePointsToClusters(std::vector<Point>* points, std::vector<Point>* c
 
 
 void initCenters(std::vector<Point>* pointlist, int ncenters, std::vector<Point>* oldcenterlist, std::vector<Point>* newcenterlist){
-//std::vector<Point> initCenters(std::vector<Point>* points, int ncenters){
- // initialize centers
  //srand(time(0));  // need to set the random seed
- //std::vector<Point> centers;
  for (int i = 0; i < ncenters; ++i) {
      oldcenterlist->push_back( Point(__DBL_MAX__,__DBL_MAX__) );
      newcenterlist->push_back(pointlist->at(rand() % pointlist->size() ));
-     //centers.push_back(points->at(rand() % points->size() ));
-     //centers.push_back(points->at(i));
+     //newcenterlist.push_back(points->at(rand() % points->size() ));
  }
- //return centers;
 }
 
 
@@ -148,28 +144,14 @@ std::vector<double> sumX, sumY;
 int main()
 {
 
- //Point a = Point(2,5);
- //Point b = Point(3,3);
- //Point c = Point(1,4);
-
  int ncenters = 2;
 
  std::string filename = "twoblobs.csv";
 
  std::vector<Point> pointlist =  getPointsFromCSV(filename);
- //std::vector<Point> pointlist;
-  //pointlist.push_back(a);
-  //pointlist.push_back(b);
-  //pointlist.push_back(c);
-  //std::cout<< pointlist.size() <<std::endl;
-
-  //  a.cluster = 3;
-  //  std::cout<< b.x <<std::endl;
-  //  std::cout<< a.x << ", "<< a.y << ", "<< a.cluster << std::endl;
 
  std::vector<Point> oldcenterlist;
  std::vector<Point> newcenterlist;
- //std::vector<Point> newcenterlist = initCenters(&pointlist, ncenters, &oldcenterlist, &newcenterlist);
  initCenters(&pointlist, ncenters, &oldcenterlist, &newcenterlist);
 
  //std::cout<<" Centers init-ed\n";
@@ -177,29 +159,64 @@ int main()
  //  std::cout << newcenterlist[i].x << " " << newcenterlist[i].y <<std::endl;
  //}
 
- std::cout<<" Unassociated points\n";
- for (std::vector<Point>::iterator it = pointlist.begin();
-      it != pointlist.end(); ++it) {
-  Point p = *it;
-  std::cout<<p.x<<" "<<p.y<<" "<<p.minDist<<" "<<p.cluster<<"\n";
-  
+ //std::cout<<" Unassociated points\n";
+ //for (std::vector<Point>::iterator it = pointlist.begin();
+ //     it != pointlist.end(); ++it) {
+ // Point p = *it;
+ // std::cout<<p.x<<" "<<p.y<<" "<<p.minDist<<" "<<p.cluster<<"\n";
+ //}
+
+
+ int iterationnr = 0;
+ while(true){
+  associatePointsToClusters(&pointlist, &newcenterlist);
+
+  //std::cout<<" Associated points\n";
+  //for (std::vector<Point>::iterator it = pointlist.begin();
+  //     it != pointlist.end(); ++it) {
+  // Point p = *it;
+  // std::cout<<p.x<<" "<<p.y<<" "<<p.minDist<<" "<<p.cluster<<"\n";
+  // 
+  //}
+
+  recalculateCenters(&pointlist, &newcenterlist);
+
+  //std::cout << newcenterlist.size() <<std::endl;
+  //for (int i = 0; i < newcenterlist.size(); ++i) {
+  //  std::cout << newcenterlist[i].x << " " << newcenterlist[i].y <<std::endl;
+  //}
+
+  double diffX = 0;
+  double diffY = 0;
+  for (int i = 0; i < newcenterlist.size(); ++i) {
+    diffX += newcenterlist[i].x - oldcenterlist[i].x;
+    diffY += newcenterlist[i].y - oldcenterlist[i].y;
   }
+  std::cout<<" Diffs "<<diffX<<" "<<diffY<<"\n";
+  std::cout<<std::abs(diffX+diffY)<<"\n";
 
- associatePointsToClusters(&pointlist, &newcenterlist);
-
- std::cout<<" Associated points\n";
- for (std::vector<Point>::iterator it = pointlist.begin();
-      it != pointlist.end(); ++it) {
-  Point p = *it;
-  std::cout<<p.x<<" "<<p.y<<" "<<p.minDist<<" "<<p.cluster<<"\n";
+  //write to file
+  std::ofstream myfile;
+  std::string filename = "outfile"+std::to_string(iterationnr)+".csv";
+  myfile.open(filename);
+  myfile << "x,y,c" << std::endl;
   
+  for (std::vector<Point>::iterator it = pointlist.begin(); 
+       it != pointlist.end(); ++it) {
+      myfile << it->x << "," << it->y << "," << it->cluster << std::endl;
   }
+  myfile.close();
 
- recalculateCenters(&pointlist, &newcenterlist);
 
- //std::cout << newcenterlist.size() <<std::endl;
- for (int i = 0; i < ncenters; ++i) {
-   std::cout << newcenterlist[i].x << " " << newcenterlist[i].y <<std::endl;
+  if(std::abs(diffX+diffY) < 0.1) break;
+
+  for (int i = 0; i < newcenterlist.size(); ++i) {
+    oldcenterlist[i].x = newcenterlist[i].x;
+    oldcenterlist[i].y = newcenterlist[i].y;
+  }
+  
+ iterationnr++;
+
  }
 
 }
