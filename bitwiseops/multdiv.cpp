5057c3f0e76bb4ea1aee423084812cfeffcc527a
Tom Perry <tomperry7@gmail.com>
2022-01-03 10:14:08 -0700

    mult or div by 2

---
+++ bitwiseops/multdiv.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+  
+int main() {
+  
+    int x = 19;
+    cout<<"x << 1 = "<< (x << 1) <<endl;
+    cout<<"x >> 1 = "<< (x >> 1) <<endl;
+    return 0;
+}
+
+ // <<  left shift = multiply by 2
+ // >>  right shift = divide by 2
