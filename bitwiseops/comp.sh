cde19f13c9c63315f60d9fffa0fdde7169df7da8
Tom Perry <tomperry7@gmail.com>
2022-01-03 10:17:39 -0700

    compile cpp functions as exe

---
+++ bitwiseops/comp.sh
@@ -0,0 +1,5 @@
+
+clang++ -std=c++11 -stdlib=libc++ bitwise.cpp        -o  bitwise.exe	  
+clang++ -std=c++11 -stdlib=libc++ multdiv.cpp        -o  multdiv.exe	  
+clang++ -std=c++11 -stdlib=libc++ oddeven.cpp        -o  oddeven.exe
+clang++ -std=c++11 -stdlib=libc++ uniqueelement.cpp  -o  uniqueelement.exe
