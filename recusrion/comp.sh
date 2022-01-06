7d286ca3cd1d352777d638020ab953607f2da7fa
Tom Perry <tomperry7@gmail.com>
2022-01-05 18:30:41 -0700

    compound interest calculator

--- recusrion/comp.sh
+++ recusrion/comp.sh
@@ -4,4 +4,5 @@
 # clang++ -std=c++11 -stdlib=libc++ sumton.cpp -o sumton
 # clang++ -std=c++11 -stdlib=libc++ countupdown.cpp -o countupdown
 # clang++ -std=c++11 -stdlib=libc++ findmax.cpp -o findmax
-clang++ -std=c++11 -stdlib=libc++ selectionsort.cpp -o selectionsort.exe
+# clang++ -std=c++11 -stdlib=libc++ selectionsort.cpp -o selectionsort.exe
+clang++ -std=c++11 -stdlib=libc++ compoundinterest.cpp -o compoundinterest.exe
