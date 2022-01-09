b2866496a9f8d032ec0bf8744ff7c0f7f5db1180
Tom Perry <tomperry7@gmail.com>
2022-01-09 09:59:59 -0700

    add coloring to points

--- meanshift/makeplot.py
+++ meanshift/makeplot.py
@@ -51,6 +51,45 @@ for x in range(0, 12):
  plt.savefig(outfilename)
  plt.clf()
  plt.close()
+
+
+# now a colored plot with centers
+x=12
+plt.figure()
+# print()"{:02d}".format(1)
+pointsfilename = filebase + "points0"
+centersfilename = filebase + "centers"+str(x)
+outfilename = filebase + "plot_" + "{:02d}".format(x) + "_color.png"
+
+
+pointsdatafile = pd.read_csv(pointsfilename+".csv")
+centersdatafile = pd.read_csv(centersfilename+".csv")
+sb.scatterplot(x=pointsdatafile.x, y=pointsdatafile.y, 
+                hue=pointsdatafile.c, 
+                palette=sb.color_palette("hls", n_colors=3))
+# sb.scatterplot(x=pointsdatafile.x, y=pointsdatafile.y, 
+#                color="black")
+sb.scatterplot(x=centersdatafile.x, y=centersdatafile.y, 
+                color="red")
+
+
+# put circles around center points  
+df = pd.read_csv(centersfilename+".csv")
+for i in range(len(df)):
+ # print(df.values[i][0]) 
+ # print(df.values[i][1]) 
+ #mycirc = plt.Circle((20, 20), 5, color='g', fill=False)
+ mycirc = plt.Circle((df.values[i][0], df.values[i][1]), 15, color='r', fill=False)
+ plt.gca().add_patch(mycirc)
+
+plt.xlabel("Some Random X")
+plt.ylabel("Some Random Y")
+plt.title("Clustered: X vs Y")
+plt.xlim([-20, 130])
+plt.ylim([-20, 100])
+plt.savefig(outfilename)
+plt.close()
+
   
  #plt.show()
 
