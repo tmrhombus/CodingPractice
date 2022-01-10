6016fc6278d32cfe921605eafc6701215fa753c9
Tom Perry <tomperry7@gmail.com>
2022-01-09 21:46:05 -0700

    add extra plots, variables for ncolors, plots

--- kmeans/makeplot.py
+++ kmeans/makeplot.py
@@ -2,32 +2,60 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sb 
 
+filebase = "./plots/outfile_"
+
+nplots=10
+ncolors=4
+
 # Before clustering
-datafile = pd.read_csv("twoblobs.csv", header=None)
+datafile = pd.read_csv("fourgauss.csv", header=None)
 datafile.columns = ["Some Random X", "Some Random Y"]
 sb.scatterplot(x=datafile["Some Random X"], 
                 y=datafile["Some Random Y"])
-plt.title("Scatterplot of X and Y")
+plt.title("Initial Plot of X and Y")
+plt.xlim([-80, 120])
+plt.ylim([-40, 80])
+plt.savefig(filebase+"initialdata.png")
 
 # After clustering
-for x in range(0, 4):
+for x in range(0, nplots):
  plt.figure()
- filebase = "./plots/outfile_"
  pointsfilename = filebase + "points"+str(x)
  centersfilename = filebase + "centers"+str(x)
- outfilename = filebase + "plot_" + str(x) + ".png"
+ #outfilename = filebase + "plot_" + str(x) + ".png"
+ outfilename = filebase + "plot_" + "{:02d}".format(x) + ".png"
 
  pointsdatafile = pd.read_csv(pointsfilename+".csv")
  centersdatafile = pd.read_csv(centersfilename+".csv")
  sb.scatterplot(x=pointsdatafile.x, y=pointsdatafile.y, 
                  hue=pointsdatafile.c, 
-                 palette=sb.color_palette("hls", n_colors=2))
+                 palette=sb.color_palette("hls", n_colors=ncolors))
  sb.scatterplot(x=centersdatafile.x, y=centersdatafile.y, 
                  color="black")
  plt.xlabel("Some Random X")
  plt.ylabel("Some Random Y")
- plt.title("Clustered: X vs Y")
+ plt.xlim([-80, 120])
+ plt.ylim([-40, 80])
+ plt.title("Clustering - k Means")
  plt.savefig(outfilename)
  
 #plt.show()
+
+# colored plot with no centers
+x=nplots
+plt.figure()
+pointsfilename = filebase + "points"+str(x)
+outfilename = filebase + "plot_" + str(x) + ".png"
+
+pointsdatafile = pd.read_csv(pointsfilename+".csv")
+sb.scatterplot(x=pointsdatafile.x, y=pointsdatafile.y, 
+                hue=pointsdatafile.c, 
+                palette=sb.color_palette("hls", n_colors=ncolors))
+plt.xlabel("Some Random X")
+plt.ylabel("Some Random Y")
+plt.xlim([-80, 120])
+plt.ylim([-40, 80])
+plt.title("Clustered - k Means")
+plt.savefig(outfilename)
+ 
 
