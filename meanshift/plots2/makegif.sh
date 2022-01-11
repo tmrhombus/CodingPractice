71062b0d1df5c864f389001b68d1c318f8a4a0d7
Tom Perry <tomperry7@gmail.com>
2022-01-10 19:19:00 -0700

    mv threegauss

---
+++ meanshift/plots2/makegif.sh
@@ -0,0 +1,28 @@
+
+convert -delay 40 -loop 0     \
+outfile_initialdata.png       \
+outfile_initialdata.png       \
+outfile_initialdata.png       \
+outfile_initialdata.png       \
+outfile_initialdata.png       \
+outfile_plot_00.png           \
+outfile_plot_01.png           \
+outfile_plot_02.png           \
+outfile_plot_03.png           \
+outfile_plot_04.png           \
+outfile_plot_05.png           \
+outfile_plot_06.png           \
+outfile_plot_07.png           \
+outfile_plot_08.png           \
+outfile_plot_09.png           \
+outfile_plot_10.png           \
+outfile_plot_11.png           \
+outfile_plot_12_color.png     \
+outfile_plot_12_colorNOC.png  \
+outfile_plot_12_colorNOC.png  \
+outfile_plot_12_colorNOC.png  \
+outfile_plot_12_colorNOC.png  \
+outfile_plot_12_colorNOC.png  \
+animation.gif
+
+
