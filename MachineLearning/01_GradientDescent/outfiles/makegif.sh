a0738012e60ba389239b8988a7d29e8fbba7fe3f
Tom Perry <tomperry7@gmail.com>
2022-01-16 12:10:06 -0700

    animation of convergence

---
+++ MachineLearning/01_GradientDescent/outfiles/makegif.sh
@@ -0,0 +1,41 @@
+
+convert -delay 20 -loop 0     \
+initialdata.png \
+initialdata.png \
+initialdata.png \
+initialdata.png \
+initialdata.png \
+fitted0000.png \
+fitted0001.png \
+fitted0002.png \
+fitted0003.png \
+fitted0004.png \
+fitted0005.png \
+fitted0006.png \
+fitted0007.png \
+fitted0008.png \
+fitted0009.png \
+fitted0010.png \
+fitted0011.png \
+fitted0012.png \
+fitted0013.png \
+fitted0014.png \
+fitted0015.png \
+fitted0016.png \
+fitted0017.png \
+fitted0018.png \
+fitted0019.png \
+fitted0020.png \
+fitted0021.png \
+fitted0022.png \
+fitted0022.png \
+fitted0022.png \
+fitted0022.png \
+fitted0022.png \
+fitted0022.png \
+fitted0022.png \
+fitted0022.png \
+fitted0022.png \
+animation.gif
+
+
