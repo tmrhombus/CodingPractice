677c79fd941c2f57fd2e3c8d255d126fac62827a
Tom Perry <tomperry7@gmail.com>
2022-01-22 21:36:15 -0700

    animation gifs

--- MachineLearning/01p5_GradientDescent/output/makeGIF_fits.sh
+++ MachineLearning/01p5_GradientDescent/output/makeGIF_fits.sh
@@ -1,17 +1,26 @@
 
 convert -delay 15 -loop 0 \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
-ng_foodcarts2d.png  \
+rawdata.png         \
+rawdata.png         \
+rawdata.png         \
+rawdata.png         \
+rawdata.png         \
+rawdata.png         \
+rawdata.png         \
+rawdata.png         \
+rawdata.png         \
+rawdata.png         \
+scaleddata.png      \
+scaleddata.png      \
+scaleddata.png      \
+scaleddata.png      \
+scaleddata.png      \
+scaleddata.png      \
+scaleddata.png      \
+scaleddata.png      \
+scaleddata.png      \
+scaleddata.png      \
+fitted0000.png      \
 fitted0015.png      \
 fitted0030.png      \
 fitted0045.png      \
@@ -45,55 +54,204 @@ fitted0450.png      \
 fitted0465.png      \
 fitted0480.png      \
 fitted0495.png      \
-fitted0510.png      \
-fitted0525.png      \
-fitted0540.png      \
-fitted0555.png      \
-fitted0570.png      \
-fitted0585.png      \
+fitted0500.png      \
+fitted0550.png      \
 fitted0600.png      \
-fitted0615.png      \
-fitted0630.png      \
-fitted0645.png      \
-fitted0660.png      \
-fitted0675.png      \
-fitted0690.png      \
-fitted0705.png      \
-fitted0720.png      \
-fitted0735.png      \
+fitted0650.png      \
+fitted0700.png      \
 fitted0750.png      \
-fitted0765.png      \
-fitted0780.png      \
-fitted0795.png      \
-fitted0810.png      \
-fitted0825.png      \
-fitted0840.png      \
-fitted0855.png      \
-fitted0870.png      \
-fitted0885.png      \
+fitted0800.png      \
+fitted0850.png      \
 fitted0900.png      \
-fitted0915.png      \
-fitted0930.png      \
-fitted0945.png      \
-fitted0960.png      \
-fitted0975.png      \
-fitted0990.png      \
-fitted1005.png      \
-fitted1020.png      \
-fitted1035.png      \
+fitted0950.png      \
+fitted1000.png      \
 fitted1050.png      \
-fitted1065.png      \
-fitted1080.png      \
-fitted1095.png      \
-fitted1096.png      \
-fitted1096.png      \
-fitted1096.png      \
-fitted1096.png      \
-fitted1096.png      \
-fitted1096.png      \
-fitted1096.png      \
-fitted1096.png      \
-fitted1096.png      \
-fitted1096.png      \
-fitted1096.png      \
+fitted1100.png      \
+fitted1150.png      \
+fitted1200.png      \
+fitted1250.png      \
+fitted1300.png      \
+fitted1350.png      \
+fitted1400.png      \
+fitted1450.png      \
+fitted1500.png      \
+fitted1600.png      \
+fitted1700.png      \
+fitted1800.png      \
+fitted1900.png      \
+fitted2000.png      \
+fitted2100.png      \
+fitted2200.png      \
+fitted2300.png      \
+fitted2400.png      \
+fitted2500.png      \
+fitted2600.png      \
+fitted2700.png      \
+fitted2800.png      \
+fitted2900.png      \
+fitted3000.png      \
+fitted3100.png      \
+fitted3200.png      \
+fitted3300.png      \
+fitted3400.png      \
+fitted3500.png      \
+fitted3600.png      \
+fitted3700.png      \
+fitted3800.png      \
+fitted3900.png      \
+fitted4000.png      \
+fitted4100.png      \
+fitted4200.png      \
+fitted4300.png      \
+fitted4400.png      \
+fitted4500.png      \
+fitted4600.png      \
+fitted4700.png      \
+fitted4800.png      \
+fitted4900.png      \
+fitted5000.png      \
+fitted5100.png      \
+fitted5200.png      \
+fitted5300.png      \
+fitted5400.png      \
+fitted5500.png      \
+fitted5600.png      \
+fitted5700.png      \
+fitted5800.png      \
+fitted5900.png      \
+fitted5999.png      \
+fitted5999.png      \
+fitted5999.png      \
+fitted5999.png      \
+fitted5999.png      \
+fitted5999.png      \
+fitted5999.png      \
+fitted5999.png      \
+fitted5999.png      \
+fitted5999.png      \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
+finalfitted.png     \
 animation_fits.gif
+
+
+
+
+
+#J_0000.png
+#J_0015.png
+#J_0030.png
+#J_0045.png
+#J_0060.png
+#J_0075.png
+#J_0090.png
+#J_0105.png
+#J_0120.png
+#J_0135.png
+#J_0150.png
+#J_0165.png
+#J_0180.png
+#J_0195.png
+#J_0210.png
+#J_0225.png
+#J_0240.png
+#J_0255.png
+#J_0270.png
+#J_0285.png
+#J_0300.png
+#J_0315.png
+#J_0330.png
+#J_0345.png
+#J_0360.png
+#J_0375.png
+#J_0390.png
+#J_0405.png
+#J_0420.png
+#J_0435.png
+#J_0450.png
+#J_0465.png
+#J_0480.png
+#J_0495.png
+#J_0500.png
+#J_0550.png
+#J_0600.png
+#J_0650.png
+#J_0700.png
+#J_0750.png
+#J_0800.png
+#J_0850.png
+#J_0900.png
+#J_0950.png
+#J_1000.png
+#J_1050.png
+#J_1100.png
+#J_1150.png
+#J_1200.png
+#J_1250.png
+#J_1300.png
+#J_1350.png
+#J_1400.png
+#J_1450.png
+#J_1500.png
+#J_1600.png
+#J_1700.png
+#J_1800.png
+#J_1900.png
+#J_2000.png
+#J_2100.png
+#J_2200.png
+#J_2300.png
+#J_2400.png
+#J_2500.png
+#J_2600.png
+#J_2700.png
+#J_2800.png
+#J_2900.png
+#J_3000.png
+#J_3100.png
+#J_3200.png
+#J_3300.png
+#J_3400.png
+#J_3500.png
+#J_3600.png
+#J_3700.png
+#J_3800.png
+#J_3900.png
+#J_4000.png
+#J_4100.png
+#J_4200.png
+#J_4300.png
+#J_4400.png
+#J_4500.png
+#J_4600.png
+#J_4700.png
+#J_4800.png
+#J_4900.png
+#J_5000.png
+#J_5100.png
+#J_5200.png
+#J_5300.png
+#J_5400.png
+#J_5500.png
+#J_5600.png
+#J_5700.png
+#J_5800.png
+#J_5900.png
+#J_5999.png
