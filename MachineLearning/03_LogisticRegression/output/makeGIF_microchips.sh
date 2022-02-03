00ff9f27ed1f86af37fb386e792ba6cc73b34afc
Tom Perry <tomperry7@gmail.com>
2022-02-02 22:47:20 -0700

    make animation gif

---
+++ MachineLearning/03_LogisticRegression/output/makeGIF_microchips.sh
@@ -0,0 +1,11 @@
+
+convert -delay 100 -loop 0   \
+microchips_initialdata.png  \
+microchips_initialdata.png  \
+microchips_finalfits.png    \
+microchips_finalfits.png    \
+microchips_finalfits.png    \
+microchips_finalfits.png    \
+microchips_finalfits.png    \
+animation_microchips.gif
+
---
+++ MachineLearning/03_LogisticRegression/output/makeGIF_testscores.sh
@@ -0,0 +1,10 @@
+
+convert -delay 100 -loop 0 \
+testscores_rawdata.png \
+testscores_rawdata.png \
+testscores_fitted.png  \
+testscores_fitted.png  \
+testscores_fitted.png  \
+testscores_fitted.png  \
+testscores_fitted.png  \
+animation_testscores.gif
