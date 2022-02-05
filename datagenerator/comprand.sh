26947d36de4c387bad8cd85c85f8a19044db8306
Tom Perry <tomperry7@gmail.com>
2022-02-04 21:10:30 -0700

    twoclasses gaussian dist

--- datagenerator/comprand.sh
+++ datagenerator/comprand.sh
@@ -1,3 +1,4 @@
 
-clang++ -std=c++11 -stdlib=libc++ gauss2d.cpp -o gauss2d.exe
+#clang++ -std=c++11 -stdlib=libc++ gauss2d.cpp -o gauss2d.exe
 #clang++ -std=c++11 -stdlib=libc++ linear2d.cpp -o linear2d.exe
+clang++ -std=c++11 -stdlib=libc++ twogaussclasses.cpp -o twogaussclasses.exe
