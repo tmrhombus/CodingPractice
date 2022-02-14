45f371dc4567edad3c6a9c76881e7035550d7404
Tom Perry <tomperry7@gmail.com>
2022-02-14 10:55:52 -0700

    search for optimal training parameters, C and gamma

--- MachineLearning/04_SupportVectorMachine/svm.py
+++ MachineLearning/04_SupportVectorMachine/svm.py
@@ -78,7 +78,8 @@ for dataset in datasets:
  from sklearn.svm import SVC
  
  # various values of C in L1 Soft Margin SVM
- Cs = [1,5,25,100]
+ Cs = [1,5]
+ #Cs = [1,5,10,25,50,75,100]
  for C in Cs:
   #plt.close("all")
   # clear plot
@@ -138,7 +139,8 @@ for dataset in datasets:
  
  
   # various values of C in L1 Soft Margin SVM
-  Gs = [1,5,25,100]
+  Gs = [1,100]
+  #Gs = [1,5,10,25,50,75,100]
   for G in Gs:
    fig, ax = plt.subplots()
   
@@ -196,96 +198,301 @@ for dataset in datasets:
 
 
 
+# New Dataset, Scan over C/Gamma for best values
+# relative to training dataset
+
+# Load Data/Matrices
+# load data, set initial matrices
+dataset="ex6data3"
+mat = loadmat("./data/{}.mat".format(dataset))
+X = mat["X"]
+y = mat["y"]
+Xval = mat["Xval"]
+yval = mat["yval"]
+
+## Plot Initial Data
+xmaxs = np.amax(X, axis=0)
+xmins = np.amin(X, axis=0)
+
+x1max = xmaxs[0]+0.01*(xmaxs[0]-xmins[0])
+x2max = xmaxs[1]+0.01*(xmaxs[1]-xmins[1])
+x1min = xmins[0]-0.01*(xmaxs[0]-xmins[0])
+x2min = xmins[1]-0.01*(xmaxs[1]-xmins[1])
+
+#x1min = -0.2
+#x1max = 4.2
+#x2min = 1.5
+#x2max = 5.0
+
+# make ndarray of [T, F, F, ...] based on y values
+accept,reject = (y==1).ravel(), (y==0).ravel()
+
+plt.scatter(X[accept,0],X[accept,1],
+            c=marker1_color,
+            marker=marker1_type,
+            s=marker1_size,
+            label="Passed"
+            )
+
+plt.scatter(X[reject,0],X[reject,1],
+            marker=marker0_type,
+            color=marker0_color,
+            facecolors=marker0_face,
+            edgecolors=marker0_edge,
+            s=marker0_size,
+            label="Failed"
+            )
+
+plt.xlim([x1min,x1max])
+plt.ylim([x2min,x2max])
+
+plt.title("Initial Dataset")
+plt.xlabel("Test 1")
+plt.ylabel("Test 2")
+plt.legend(loc="upper right")
+
+plt.savefig("./output/{}_initialdata.png".format(dataset), facecolor="w")
+plt.close("all")
 
 
 
-# #
-# #
-# ### SVM with Gaussian Kernel
-# #mat2 = loadmat("./data/ex6data2.mat")
-# #X2 = mat2["X"]
-# #y2 = mat2["y"]
-# #
-# #
-# #m2,n2 = X2.shape[0],X2.shape[1]
-# #pos2,neg2= (y2==1).reshape(m2,1), (y2==0).reshape(m2,1)
-# #plt.figure(figsize=(8,6))
-# #plt.scatter(X2[pos2[:,0],0],X2[pos2[:,0],1],c="r",marker="+")
-# #plt.scatter(X2[neg2[:,0],0],X2[neg2[:,0],1],c="y",marker="o")
-# #plt.xlim(0,1)
-# #plt.ylim(0.4,1)
-# #
-# #
-# #
-# #classifier3 = SVC(kernel="rbf",gamma=30)
-# #classifier3.fit(X2,y2.ravel())
-# #
-# #
-# #plt.figure(figsize=(8,6))
-# #plt.scatter(X2[pos2[:,0],0],X2[pos2[:,0],1],c="r",marker="+")
-# #plt.scatter(X2[neg2[:,0],0],X2[neg2[:,0],1],c="y",marker="o")
-# #
-# #
-# ## plotting the decision boundary
-# #X_5,X_6 = np.meshgrid(np.linspace(X2[:,0].min(),X2[:,1].max(),num=100),np.linspace(X2[:,1].min(),X2[:,1].max(),num=100))
-# #plt.contour(X_5,X_6,classifier3.predict(np.array([X_5.ravel(),X_6.ravel()]).T).reshape(X_5.shape),1,colors="b")
-# #plt.xlim(0,1)
-# #plt.ylim(0.4,1)
-# #
-# #
-# #
-# #mat3 = loadmat("./data/ex6data3.mat")
-# #X3 = mat3["X"]
-# #y3 = mat3["y"]
-# #Xval = mat3["Xval"]
-# #yval = mat3["yval"]
-# #
-# #m3,n3 = X3.shape[0],X3.shape[1]
-# #pos3,neg3= (y3==1).reshape(m3,1), (y3==0).reshape(m3,1)
-# #plt.figure(figsize=(8,6))
-# #plt.scatter(X3[pos3[:,0],0],X3[pos3[:,0],1],c="r",marker="+",s=50)
-# #plt.scatter(X3[neg3[:,0],0],X3[neg3[:,0],1],c="y",marker="o",s=50)
-# #
-# #
-# #def dataset3Params(X, y, Xval, yval,vals):
-# #    """
-# #    Returns your choice of C and sigma. You should complete this function to return the optimal C and 
-# #    sigma based on a cross-validation set.
-# #    """
-# #    acc = 0
-# #    best_c=0
-# #    best_gamma=0
-# #    for i in vals:
-# #        C= i
-# #        for j in vals:
-# #            gamma = 1/j
-# #            classifier = SVC(C=C,gamma=gamma)
-# #            classifier.fit(X,y)
-# #            prediction = classifier.predict(Xval)
-# #            score = classifier.score(Xval,yval)
-# #            if score>acc:
-# #                acc =score
-# #                best_c =C
-# #                best_gamma=gamma
-# #    return best_c, best_gamma
-# #
-# #
-# #vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
-# #C, gamma = dataset3Params(X3, y3.ravel(), Xval, yval.ravel(),vals)
-# #classifier4 = SVC(C=C,gamma=gamma)
-# #classifier4.fit(X3,y3.ravel())
-# #
-# #
-# #
-# #plt.figure(figsize=(8,6))
-# #plt.scatter(X3[pos3[:,0],0],X3[pos3[:,0],1],c="r",marker="+",s=50)
-# #plt.scatter(X3[neg3[:,0],0],X3[neg3[:,0],1],c="y",marker="o",s=50)
-# #
-# ## plotting the decision boundary
-# #X_7,X_8 = np.meshgrid(np.linspace(X3[:,0].min(),X3[:,1].max(),num=100),np.linspace(X3[:,1].min(),X3[:,1].max(),num=100))
-# #plt.contour(X_7,X_8,classifier4.predict(np.array([X_7.ravel(),X_8.ravel()]).T).reshape(X_7.shape),1,colors="b")
-# #plt.xlim(-0.6,0.3)
-# #plt.ylim(-0.7,0.5)
-# #
-# #
-# #
+def findBestParams(X,y,Xval,yval,values):
+ bestscore = 0
+ bestC     = 0
+ bestG     = 0
+ for C in values:
+  for Ginv in values:
+   G = 1/Ginv
+   
+   # make/train/score classifier
+   classifier = SVC(C=C, gamma=G, kernel="rbf")
+   classifier.fit(X,y)
+   prediction = classifier.predict(Xval)
+   score = classifier.score(Xval,yval)
+  
+   # plot the original data
+
+   # make ndarray of [T, F, F, ...] based on y values
+   accept,reject = (y==1).ravel(), (y==0).ravel()
+
+   plt.scatter(X[accept,0],X[accept,1],
+               c=marker1_color,
+               marker=marker1_type,
+               s=marker1_size,
+               label="Passed"
+               )
+   
+   plt.scatter(X[reject,0],X[reject,1],
+               marker=marker0_type,
+               color=marker0_color,
+               facecolors=marker0_face,
+               edgecolors=marker0_edge,
+               s=marker0_size,
+               label="Failed"
+               )
+   
+   plt.xlim([x1min,x1max])
+   plt.ylim([x2min,x2max])
+  
+   plt.xlabel("Test 1")
+   plt.ylabel("Test 2")
+   plt.legend(loc="upper right")
+  
+   # add SVM decision boundry line to plot
+   # make grid of input x1s and x2s
+   x1linspace=np.linspace(x1min,x1max,100)
+   x2linspace=np.linspace(x2min,x2max,100)
+   x1grid,x2grid = np.meshgrid(x1linspace, x2linspace)
+   
+   # SVM takes (x1, x2) values so unravel and combine xs
+   inputx1s = x1grid.ravel()
+   inputx2s = x2grid.ravel()
+   inputx = np.vstack([inputx1s,inputx2s]).T
+   
+   # zs for contour must match dimension of grid, hence reshape again
+   plt.contour(x1grid,x2grid,classifier.predict(inputx).reshape(x1grid.shape),1,colors="b")
+   
+   plt.title("RBF Kernel - Training Dataset")
+  
+   t1 = plt.text(0.05,0.05,"C = {}".format(C),transform=ax.transAxes)
+   t2 = plt.text(0.05,0.15,"$\gamma$ = {:03.2f}".format(G),transform=ax.transAxes)
+   #t1 = plt.text(0.05,0.05,"C = {}".format(C),transform=ax.transAxes)
+   #t2 = plt.text(0.05,0.15,"$\gamma$ = {03.2f}".format(G),transform=ax.transAxes)
+   t1.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+   t2.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+  
+   plt.savefig("./output/{}_scan_C{:03.2f}_gamma{:03.2f}_train.png".format(dataset,C,G), facecolor="w")
+   plt.close("all")
+
+   # plot validation dataset
+
+   # make ndarray of [T, F, F, ...] based on y values
+   accept,reject = (yval==1).ravel(), (yval==0).ravel()
+
+   plt.scatter(Xval[accept,0],Xval[accept,1],
+               c=marker1_color,
+               marker=marker1_type,
+               s=marker1_size,
+               label="Passed"
+               )
+   
+   plt.scatter(Xval[reject,0],Xval[reject,1],
+               marker=marker0_type,
+               color=marker0_color,
+               facecolors=marker0_face,
+               edgecolors=marker0_edge,
+               s=marker0_size,
+               label="Failed"
+               )
+   
+   plt.xlim([x1min,x1max])
+   plt.ylim([x2min,x2max])
+  
+   plt.xlabel("Test 1")
+   plt.ylabel("Test 2")
+   plt.legend(loc="upper right")
+
+   plt.contour(x1grid,x2grid,classifier.predict(inputx).reshape(x1grid.shape),1,colors="b")
+   
+   plt.title("RBF Kernel - Validation Dataset")
+  
+   t1 = plt.text(0.05,0.05,"C = {}".format(C),transform=ax.transAxes)
+   t2 = plt.text(0.05,0.15,"$\gamma$ = {:03.2f}".format(G),transform=ax.transAxes)
+   t3 = plt.text(0.05,0.25,"Score = {}".format(score),transform=ax.transAxes)
+   #t2 = plt.text(0.05,0.15,"$\gamma$ = {3.2f}".format(G),transform=ax.transAxes)
+   #t3 = plt.text(0.05,0.25,"Score = {3.2f}".format(score),transform=ax.transAxes)
+   t1.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+   t2.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+   t3.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+  
+   plt.savefig("./output/{}_scan_C{:03.2f}_gamma{:03.2f}_validate.png".format(dataset,C,G), facecolor="w")
+   plt.close("all")
+   
+   if ( score > bestscore ):
+    bestscore = score
+    bestC     = C
+    bestG     = G
+
+ return bestC, bestG, bestscore
+
+
+values = [0.01,0.03,0.1,0.3,1,3,10,30]
+C,G,score = findBestParams(X,y.ravel(),Xval,yval.ravel(),values)
+
+bestclassifier = SVC(C=C, gamma=G, kernel="rbf")
+bestclassifier.fit(X,y.ravel)
+
+# plot it
+
+# make ndarray of [T, F, F, ...] based on y values
+accept,reject = (y==1).ravel(), (y==0).ravel()
+
+plt.scatter(X[accept,0],X[accept,1],
+            c=marker1_color,
+            marker=marker1_type,
+            s=marker1_size,
+            label="Passed"
+            )
+
+plt.scatter(X[reject,0],X[reject,1],
+            marker=marker0_type,
+            color=marker0_color,
+            facecolors=marker0_face,
+            edgecolors=marker0_edge,
+            s=marker0_size,
+            label="Failed"
+            )
+
+plt.xlim([x1min,x1max])
+plt.ylim([x2min,x2max])
+
+plt.xlabel("Test 1")
+plt.ylabel("Test 2")
+plt.legend(loc="upper right")
+
+# add SVM decision boundry line to plot
+# make grid of input x1s and x2s
+x1linspace=np.linspace(x1min,x1max,100)
+x2linspace=np.linspace(x2min,x2max,100)
+x1grid,x2grid = np.meshgrid(x1linspace, x2linspace)
+
+# SVM takes (x1, x2) values so unravel and combine xs
+inputx1s = x1grid.ravel()
+inputx2s = x2grid.ravel()
+inputx = np.vstack([inputx1s,inputx2s]).T
+
+# zs for contour must match dimension of grid, hence reshape again
+plt.contour(x1grid,x2grid,bestclassifier.predict(inputx).reshape(x1grid.shape),1,colors="b")
+
+plt.title("Best Classifier on Training Dataset")
+
+t1 = plt.text(0.05,0.05,"C = {}".format(C),transform=ax.transAxes)
+t2 = plt.text(0.05,0.15,"$\gamma$ = {:03.2f}".format(G),transform=ax.transAxes)
+t3 = plt.text(0.05,0.25,"Score = {}".format(score),transform=ax.transAxes)
+t1.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+t2.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+t3.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+
+plt.savefig("./output/{}_best_train.png".format(dataset), facecolor="w")
+plt.close("all")
+
+
+
+# Plot best validation
+
+# make ndarray of [T, F, F, ...] based on y values
+accept,reject = (yval==1).ravel(), (yval==0).ravel()
+
+plt.scatter(Xval[accept,0],Xval[accept,1],
+            c=marker1_color,
+            marker=marker1_type,
+            s=marker1_size,
+            label="Passed"
+            )
+
+plt.scatter(Xval[reject,0],Xval[reject,1],
+            marker=marker0_type,
+            color=marker0_color,
+            facecolors=marker0_face,
+            edgecolors=marker0_edge,
+            s=marker0_size,
+            label="Failed"
+            )
+
+plt.xlim([x1min,x1max])
+plt.ylim([x2min,x2max])
+
+plt.xlabel("Test 1")
+plt.ylabel("Test 2")
+plt.legend(loc="upper right")
+
+# add SVM decision boundry line to plot
+# make grid of input x1s and x2s
+x1linspace=np.linspace(x1min,x1max,100)
+x2linspace=np.linspace(x2min,x2max,100)
+x1grid,x2grid = np.meshgrid(x1linspace, x2linspace)
+
+# SVM takes (x1, x2) values so unravel and combine xs
+inputx1s = x1grid.ravel()
+inputx2s = x2grid.ravel()
+inputx = np.vstack([inputx1s,inputx2s]).T
+
+# zs for contour must match dimension of grid, hence reshape again
+plt.contour(x1grid,x2grid,bestclassifier.predict(inputx).reshape(x1grid.shape),1,colors="b")
+
+plt.title("Best Classifier on Validation Dataset")
+
+t1 = plt.text(0.05,0.05,"C = {}".format(C),transform=ax.transAxes)
+t2 = plt.text(0.05,0.15,"$\gamma$ = {:03.2f}".format(G),transform=ax.transAxes)
+t3 = plt.text(0.05,0.25,"Score = {}".format(score),transform=ax.transAxes)
+t1.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+t2.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+t3.set_bbox(dict(facecolor='white', alpha=1, edgecolor='red'))
+
+plt.savefig("./output/{}_best_validate.png".format(dataset), facecolor="w")
+plt.close("all")
+
+
+
+
+
