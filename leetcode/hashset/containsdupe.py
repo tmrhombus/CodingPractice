52155d79f1aa1eef1831d97ecdc1725db08cd8e2
Tom Perry <tomperry7@gmail.com>
2022-03-15 17:52:07 -0600

    code setup

--- leetcode/hashset/containsdupe.py
+++ leetcode/hashset/containsdupe.py
@@ -4,6 +4,18 @@
 
 import myHashSet as mhs
 
-hs = mhs.HashSet()
+def containsdupes(inputarray=[]):
+
+ dupefound = False	
+ hs = mhs.HashSet()
+ 
+
+ return dupefound
 
 
+
+#### Testing
+myarray = [1,2,3,4,1]
+
+print("Input array: {}".format(myarray))
+print(" Has duplicates: {}".format(containsdupes(myarray)))
