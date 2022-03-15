756ac950378625071351dd2ad88d7481a3f07634
Tom Perry <tomperry7@gmail.com>
2022-03-14 21:09:45 -0600

    add commentary for HashSet implementation

--- leetcode/hashset/myHashSet.py
+++ leetcode/hashset/myHashSet.py
@@ -1,15 +1,29 @@
-#Design HashSet in python
-#checking the values and will return the output class
+#!/bin/python
 
-# These are the values in the hash table
-# each one is itself an array to resolve collisions
 class checkingvalues:
-   #initialization function which has list klist
+'''
+Helper class for HashSet class
+
+These are the entries in the hash table
+Each entry is itself an array (named klist) to 
+ handle collisions of multiple values hashing 
+ to the same klist
+Functions are defined to
+ update: add a value to klist
+ get: check if value is in klist
+ remove: remove a value from klist
+'''
+   #initialization function to define array klist
    def __init__(self):
       self.klist=[]
      
    #update vales function
    def update(self, key):
+   '''
+   Go through klist, looking for key
+   If key is already in klist, do nothing
+   If key isn't in klist, add it to the end
+   '''
       found=False
       for i,k in enumerate(self.klist):
          if key==k:
@@ -21,6 +35,10 @@ class checkingvalues:
     
    #get values function
    def get(self, key):
+   '''
+   return True if key in klist
+          False if key isn't there
+   '''
       for k in self.klist:
          if k==key:
             return True
@@ -28,43 +46,59 @@ class checkingvalues:
 
    #remove values function
    def remove(self, key):
+   '''
+   if key is in klist, delete it
+   '''
       for i,k in enumerate(self.klist):
          if key==k:
             del self.klist[i]
  
-#class HashSet main class
+# The actual HashSet class
 class HashSet:
    #Initialization function
    def __init__(self):
+      '''
+      Initialize hash_table as an array of length key_space 
+       with an empty checkingvalues() at each entry of array
+      '''
       self.key_space = 2096 # how long the hash table is
-      # h_t is an array of length key_space, initialized with
-      # that many checkingvalues objects
       self.hash_table=[checkingvalues() for i in range(self.key_space)]
 
    # make hash_key from value you give it, just mod by length of h_t
    def hash_values(self, key):
+       '''
+       The hash_table only has key_space buckets, so use mod(key_space)
+        on input key to determine which bucket it goes to
+       Bucket number is the hash_key
+       '''
        hash_key=key%self.key_space
        return hash_key
 
    #add function
    def add(self, key):
+   ''' add key to hash_table in location key % key_space '''
       self.hash_table[self.hash_values(key)].update(key)
 
    #remove function
    def remove(self, key):
+   ''' remove key from hash_table '''
       self.hash_table[self.hash_values(key)].remove(key)
 
    #contains function
    def contains(self, key): 
+   ''' check if klist contains key '''
       return self.hash_table[self.hash_values(key)].get(key)
 
    def display(self):
+   ''' print the full hash_table '''
        ls=[]
        for i in self.hash_table:
            if len(i.klist)!=0:ls.append(i.klist[0])
        print(ls)
   
     
+
+### Checks
 print(" {}".format( checkingvalues() ))
 hash_table=[checkingvalues() for i in range(10)]
 print(" {}".format( hash_table ) )
