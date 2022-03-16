454de4d10da657db7269f12ec1e3b3e0c508061e
Tom Perry <tomperry7@gmail.com>
2022-03-15 19:28:28 -0600

    hash shorter array

--- leetcode/hashset/arrayintersect.py
+++ leetcode/hashset/arrayintersect.py
@@ -8,9 +8,29 @@ import myHashSet as mhs
 def intersection(arr1=[], arr2=[]):
  arrintersect=[]
 
+ hs = mhs.HashSet()
+ if(len(arr1)<len(arr2)):
+  shortarr = arr1
+  longarr  = arr2
+ else:
+  shortarr = arr2
+  longarr  = arr1
+  
+
+ for num in shortarr:
+  hs.add(num)
+
+ hs.display()
+
  return arrintersect
 
 
 
 array1 = [1,2,3,4,5]
 array2 = [4,5,6,7,8]
+
+print("Input arrays:")
+print(" {}".format(array1))
+print(" {}".format(array2))
+
+arrintersect = intersection(array1, array2)
