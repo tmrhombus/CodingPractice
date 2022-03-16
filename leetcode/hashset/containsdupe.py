13b0bf095fcb7fcf01c83ecbe186e066cf6d78ef
Tom Perry <tomperry7@gmail.com>
2022-03-15 18:07:19 -0600

    name the first duplicate element found

--- leetcode/hashset/containsdupe.py
+++ leetcode/hashset/containsdupe.py
@@ -4,18 +4,31 @@
 
 import myHashSet as mhs
 
-def containsdupes(inputarray=[]):
+def dupesearch(inputarray=[]):
 
  dupefound = False	
+ thedupe = None
  hs = mhs.HashSet()
- 
 
- return dupefound
+ itnr = 0
+ while itnr<len(inputarray):
+
+  if(not hs.contains(inputarray[itnr])):
+   hs.add(inputarray[itnr])
+  else:
+   dupefound=True
+   thedupe = inputarray[itnr]
+   break
+
+  itnr += 1
+
+ return dupefound, thedupe
 
 
 
 #### Testing
-myarray = [1,2,3,4,1]
+myarray = [1,2,3,4,1,6,7,8]
 
+dupefound, thedupe = dupesearch(myarray)
 print("Input array: {}".format(myarray))
-print(" Has duplicates: {}".format(containsdupes(myarray)))
+print(" Has duplicates: {} ({})".format(dupefound, thedupe))
