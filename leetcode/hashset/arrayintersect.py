7a113a5edfeb8e09684806c18584035d42747e6b
Tom Perry <tomperry7@gmail.com>
2022-03-15 19:20:40 -0600

    intersection of arrays problem statement

---
+++ leetcode/hashset/arrayintersect.py
@@ -0,0 +1,16 @@
+
+## Problem Statement
+## Given two integer arrays, find their intersection
+# Each element in the result must be unique and you may return the result in any order.
+
+import myHashSet as mhs
+
+def intersection(arr1=[], arr2=[]):
+ arrintersect=[]
+
+ return arrintersect
+
+
+
+array1 = [1,2,3,4,5]
+array2 = [4,5,6,7,8]
