da039594d26c0752325d6879ebcee5bcb85369c1
Tom Perry <tomperry7@gmail.com>
2022-03-17 17:25:57 -0600

    breadth first search algo

--- trees/binarytree.py
+++ trees/binarytree.py
@@ -117,4 +117,21 @@ class BinaryTree:
  #   7        print 7
 
 
+def bfs(self):
+ queue = Queue()
+ queue.put(self)
+
+ while not queue.empty():
+  current_node = queue.get()
+  print(current_node.value)
+
+  if current_node.left_child:
+   queue.put(current_node.left_child)
+
+  if current_node.right_child:
+   queue.put(current_node.right_child)
+
+
+
+
 
