28169fcd6c7ae1d7a9a05af8be424f94dfd67c64
Tom Perry <tomperry7@gmail.com>
2022-03-18 20:31:02 -0600

    move test to its own file

--- graphs/topologicalsort.py
+++ graphs/topologicalsort.py
@@ -74,14 +74,3 @@ class Graph:
             # Print topological order
             print (top_order)
  
- 
-g = Graph(6)
-g.addEdge(5, 2);
-g.addEdge(5, 0);
-g.addEdge(4, 0);
-g.addEdge(4, 1);
-g.addEdge(2, 3);
-g.addEdge(3, 1);
- 
-print ("Following is a Topological Sort of the given graph")
-g.topologicalSort()
