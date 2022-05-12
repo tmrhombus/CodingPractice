7a3dfb8b263cace18dce37bae06d9b5cca14b9f5
Tom Perry <tomperry7@gmail.com>
2022-05-11 20:37:40 -0600

    return empty list if it's too short

--- leetcode/4sum/solution.py
+++ leetcode/4sum/solution.py
@@ -11,12 +11,24 @@ def solution( nums, target ):
 
  # object to be returned, list of lists
  outlist = []
+
+ # check if solutions are possible
+ if nums is None or len(nums)<4:
+  return outlist
  
  # start by sorting the list
- nums.sort()
- n = len(nums)
+ nums = sorted(set(nums))
+ # n = index of final element in nums
+ n = len(nums)-1
 
+ """
+ Idea behind algo:
+ indexes of elements are i,j,k,l
+ index of final element is n
+
+ start with i=0, j=1,k=
+ """
 
  print(nums)
  print(nums[0])
- print(nums[n-1])
+ print(nums[n])
