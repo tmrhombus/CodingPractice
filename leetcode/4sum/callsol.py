6e86680b6bd66ee2c7f4280849053f38e11d137e
Tom Perry <tomperry7@gmail.com>
2022-05-13 20:02:36 -0600

    print solution

--- leetcode/4sum/callsol.py
+++ leetcode/4sum/callsol.py
@@ -1,6 +1,12 @@
 import solution as sol
 
-nums = [1,3,5,7,2,4,6,8]
+nums = [1,3,5,7,2,4,6,8,7,2,4,6,8]
 target = 11
 
-sol.solution( nums, target )
+outlist = sol.solution( nums, target )
+
+print("Initial list: \n {}".format(nums))
+print("\n Target: {}".format(target))
+print("\n Valid Combinations:")
+for alist in outlist:
+ print(" {}".format(alist))
