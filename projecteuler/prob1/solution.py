aaf8669232facbebaaeae5648175b4fcf62b3390
Tom Perry <tomperry7@gmail.com>
2022-05-13 20:18:46 -0600

    implementation with two loops

---
+++ projecteuler/prob1/solution.py
@@ -0,0 +1,29 @@
+
+
+def solution( target ):
+ """
+ returns the sum of all multiples of 3 or 5 
+ below target
+ """
+
+ # initialize hashset
+ s = set()
+
+ i=1
+ while True:
+  if 3*i <= target:
+   s.add(3*i)
+   i += 1
+  else:
+   break
+
+ j=1
+ while True:
+  if 5*j <= target:
+   s.add(5*j)
+   j += 1
+  else:
+   break
+
+
+
