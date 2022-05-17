c081bd19ed9171cbb989607fa6d816e9b2b78d2b
Tom Perry <tomperry7@gmail.com>
2022-05-16 19:58:14 -0600

    algo defined

--- projecteuler/prob3/solution.py
+++ projecteuler/prob3/solution.py
@@ -3,7 +3,35 @@
 def solution( target ):
  """
  returns the largest prime factor of target
+
+ method:
+
+ find A = lowest factor of target by checking 2,3,4,...
+ split target into t = A*B 
+  A is prime, add it to set of primes
+  B becomes new target
+   repeat iteraticely
+
+  if we can not find any factors for target, we're done
+ 
  """
 
- factors = 
+ #factors = set( target )
+ pfactors = set()
+
  
+ 
+ 
+
+def findminfactor( x ):
+ """
+ find first factor of x
+ """
+
+ i=2
+ while i<x:
+  
+
+
+
+
