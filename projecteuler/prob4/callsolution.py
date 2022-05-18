abfdd272a80f1af89d3b1c5259952049b94b0072
Tom Perry <tomperry7@gmail.com>
2022-05-18 16:22:02 -0600

    check on 3x3 -digit

--- projecteuler/prob4/callsolution.py
+++ projecteuler/prob4/callsolution.py
@@ -1,8 +1,16 @@
 import solution as sol
+import ndigitsol as nd
 
 
-i,j,themax = sol.solution()
+#i,j,themax = sol.solution()
+#
+#print("Largest 3x3-digit palendrome is {} x {} = {}".format(i,j,themax))
 
-print("Largest 3-digit palendrome is {} x {} = {}".format(i,j,themax))
+idig = 3
+jdig = 3
+
+i,j,themax = nd.solution( idig, jdig )
+
+print("Largest {}x{}-digit palendrome is {} x {} = {}".format(idig, jdig, i, j, themax))
 
 
