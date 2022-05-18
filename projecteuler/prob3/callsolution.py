cc118c616a512f185c425419a152e65940bacf85
Tom Perry <tomperry7@gmail.com>
2022-05-17 21:46:10 -0600

    done with test cases - find solution for real target

--- projecteuler/prob3/callsolution.py
+++ projecteuler/prob3/callsolution.py
@@ -4,3 +4,8 @@ target = 10
 themax = sol.solution(target)
 
 print("Max prime factor for {} is {}".format(target,themax))
+
+target = 600851475143
+themax = sol.solution(target)
+
+print("Max prime factor for {} is {}".format(target,themax))
