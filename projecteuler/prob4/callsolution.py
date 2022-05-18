c5b6814a9c557fc7ca4deec3228e3c9b7dce00cf
Tom Perry <tomperry7@gmail.com>
2022-05-18 16:25:06 -0600

    run over a variety of digit length combinations

--- projecteuler/prob4/callsolution.py
+++ projecteuler/prob4/callsolution.py
@@ -6,11 +6,10 @@ import ndigitsol as nd
 #
 #print("Largest 3x3-digit palendrome is {} x {} = {}".format(i,j,themax))
 
-idig = 3
-jdig = 3
 
-i,j,themax = nd.solution( idig, jdig )
-
-print("Largest {}x{}-digit palendrome is {} x {} = {}".format(idig, jdig, i, j, themax))
+for idig in range(1,5):
+ for jdig in range(1,4):
+  i,j,themax = nd.solution( idig, jdig )
+  print("Largest {}x{}-digit palendrome is {} x {} = {}".format(idig, jdig, i, j, themax))
 
 
