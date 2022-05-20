3cc4bc87d0c0bb9137f1a69af3bb7a7a0ab5a67f
Tom Perry <tomperry7@gmail.com>
2022-05-20 04:08:43 -0600

    method for solution

---
+++ projecteuler/prob6/solution.py
@@ -0,0 +1,14 @@
+
+def solution( b, t ):
+ """
+
+ returns the difference between the sum of the squares
+ and the square of the sum of all numbers in range
+ from b to t
+
+ method:
+
+ just compute them, nothing special
+
+ """
+
