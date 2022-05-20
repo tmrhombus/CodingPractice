3423845f692bc72446a88e4d26b7843d5dc124c0
Tom Perry <tomperry7@gmail.com>
2022-05-20 04:10:15 -0600

    bf solution

---
+++ projecteuler/prob6/callsolution.py
@@ -0,0 +1,10 @@
+import solution as sol
+
+
+ 
+n=10 
+
+ans  = sol.solution( n )
+ 
+print("\n\nThe difference between the square of the sum and the sum of squares in the range from 1 to {} is {}  \n\n".format(n, ans))
+  
--- projecteuler/prob6/solution.py
+++ projecteuler/prob6/solution.py
@@ -1,5 +1,5 @@
 
-def solution( b, t ):
+def solution( n ):
  """
 
  returns the difference between the sum of the squares
@@ -8,7 +8,28 @@ def solution( b, t ):
 
  method:
 
+ A: 
  just compute them, nothing special
 
+ B: Faulhaber's formula
+    Sum (k)   from 1 to n = n(n+1)/2
+    Sum (k^2) from 1 to n = n(n+1)(2n+1)/6
+ 
  """
 
+ doprint = True
+
+ # Method A
+ a = 0
+ a2 = 0
+ for i in range(1,n+1):
+  a  = a + i
+  a2 = a2 + i**2
+
+ sum2a = a**2
+ if(doprint):
+  print(sum2a)
+  print(a2)
+ diff = sum2a - a2
+
+ return diff
