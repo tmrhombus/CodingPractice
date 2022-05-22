5811f356d9f4b2ae1323a07a15a4b622cff08153
Tom Perry <tomperry7@gmail.com>
2022-05-22 16:11:41 -0600

    loop until prime list is n entries long

--- projecteuler/prob7/solution.py
+++ projecteuler/prob7/solution.py
@@ -9,17 +9,29 @@ def solution( n ):
  Incremental Sieve of Eratosthenes  
 
  store list of primes and multiple of primes
- loop over numbers [n]
+ loop over numbers [x]
   loop over primes [p]
-   find first multiple of p that's larger than n
-   if a multiple of p equals n, go to next n
+   find first multiple of p that's larger than x
+   if a multiple of p equals x, go to next x
   if you go through all primes and haven't found a multiple
-   add n to list of primes
+   add x to list of primes
 
  """
 
- doprint = True
-
- primelist = []
-
- return primelist
+ primes = [ [2, 2] ]
+ 
+ x=3
+ while len(primes) < n:
+ #for x in range(3, 100000) :
+  isprime = True;
+  for pm in primes :
+   while pm[1] < x : 
+    pm[1] += pm[0]
+   if pm[1] == x : 
+    isprime = False;
+    break
+  if isprime :
+   primes.append([x, x]);
+  x = x+1
+ 
+ return primes
