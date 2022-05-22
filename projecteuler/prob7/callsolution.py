31d45369135a6b8526e90c39bcd178f2181df026
Tom Perry <tomperry7@gmail.com>
2022-05-22 16:20:46 -0600

    print out just prime, not prime/multiple pair

--- projecteuler/prob7/callsolution.py
+++ projecteuler/prob7/callsolution.py
@@ -2,13 +2,13 @@ import solution as sol
 
  
 # n = number of primes to list
-n = 5
+n = 10001
 
 ans = sol.solution( n )
   
-print("\n\nPrime number number  {} is {} \n\n".format(n, ans[-1]))
+print("\n\nPrime number number  {} is {} \n\n".format(n, ans[-1][0]))
 
-print("The full list of primes is:")
-for p in ans:
- print(" {}".format(p))
+#print("The full list of primes is:")
+#for p in ans:
+# print(" {}".format(p))
   
