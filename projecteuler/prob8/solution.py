4dd2be31fc22be44fdbc755a7ccd57d289ef89b8
Tom Perry <tomperry7@gmail.com>
2022-05-23 09:02:36 -0600

    skeleton of idea

---
+++ projecteuler/prob8/solution.py
@@ -0,0 +1,15 @@
+import math 
+
+def solution( n, x ):
+ """
+
+ finds and returns the string of n digits in x
+ which multiply to give the greatest product
+
+ method:
+ brute force: sliding window 15 wide, check them all
+
+ """
+
+
+ return digits, prod
