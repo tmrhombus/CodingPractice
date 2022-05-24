c35096d8c8d6bc151f038b9e00ccfbf46b5fd608
Tom Perry <tomperry7@gmail.com>
2022-05-24 13:25:09 -0600

    full solution

--- projecteuler/prob9/solution.py
+++ projecteuler/prob9/solution.py
@@ -13,10 +13,12 @@ def solution( p ):
  trips = []
  for a in range( 1,p ):
   for b in range( 1,p-a ):
-   print(" {} + {} = {}".format(a,b,a+b))
+   #print(" {} + {} = {}".format(a,b,a+b))
    c = p - a - b
-   print(" a + b + c = {}".format(a+b+c))
-   print(" {} + {} + {} = {}".format(a,b,c,a+b+c))
-   print("--------------")
+   #print(" a + b + c = {}".format(a+b+c))
+   #print(" {} + {} + {} = {}".format(a,b,c,a+b+c))
+   #print("--------------")
+   if (a**2+b**2==c**2):
+    trips.append([a,b,c])
 
  return trips
