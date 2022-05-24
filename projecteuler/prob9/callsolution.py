95bad9ed5a70b692c07df4443fce9041fa8804aa
Tom Perry <tomperry7@gmail.com>
2022-05-24 13:26:33 -0600

    sum=1000, solution

--- projecteuler/prob9/callsolution.py
+++ projecteuler/prob9/callsolution.py
@@ -1,11 +1,12 @@
 import solution as sol
 
 # p = the desired perimeter of the triangle
-p = 12
+p = 1000
 
 trips = sol.solution( p )
   
 print("\n\nThe Pythagorean triplets that sum to {} are:".format(p))
 for t in trips:
  print(" {}".format(t))
+
 
