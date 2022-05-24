7f7cec0b013629d2a79c9612a876f400af10000a
Tom Perry <tomperry7@gmail.com>
2022-05-24 13:22:04 -0600

    setup function

---
+++ projecteuler/prob9/solution.py
@@ -0,0 +1,16 @@
+import math 
+
+def solution( p ):
+ """
+ returns the list of pythagorean triplets which
+  sum to p
+
+ method: brute force 
+  loop through a,b
+  check if c=p-a-b completes the triplet
+ """
+
+ # cast x to a string for easy indexing
+ trips = []
+
+ return trips
