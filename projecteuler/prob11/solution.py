1a6600fb44c96ca280c7e5136a87c1760254bbe5
Tom Perry <tomperry7@gmail.com>
2022-05-29 09:23:55 -0600

    loop structure

--- projecteuler/prob11/solution.py
+++ projecteuler/prob11/solution.py
@@ -10,6 +10,10 @@ def solution( n, grid ):
  at grid point (i,j) only check right,down,downward diagonals
  """
 
+ for i in range(len(grid[0])):
+  for j in range(len(grid[0])):
+   print(grid[i][j])
+
  index = [-1,-1]
  nums = [-1,-1,-1,5]
 
