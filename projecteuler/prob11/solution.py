4b2ed1ca3b0fb3381a191a4b3b7afaab3e2754ee
Tom Perry <tomperry7@gmail.com>
2022-05-29 10:43:09 -0600

    rotate grid coordinate system

--- projecteuler/prob11/solution.py
+++ projecteuler/prob11/solution.py
@@ -6,22 +6,25 @@ def solution( n, grid ):
  where sequential can mean horizontal, vertical, diagonal
 
  method:
- work top to bottom, left to right
+ use rotated coordinate system
  at grid point (i,j) only check right,down,downward diagonals
  """
 
  maxprod = -1
- hlen = len(grid[0])
- vlen = len(grid)
+ hlen = len(grid)
+ vlen = len(grid[0])
 
- for j in range(vlen):
-  for i in range(hlen):
+ for i in range(hlen):
+  for j in range(vlen):
 
    # check horisontally
    if (i <= hlen - n):
+    print(" ({},{})={}".format(i,j,grid[i][j]))
+    print("  {}".format(prod(i,j,1,0,n,grid)))
     #print(grid)
-    print(i)
+    #print(i)
    #print(grid[i][j])
+  print("\n")
 
  index = [-1,-1]
  nums = [-1,-1,-1,5]
@@ -33,5 +36,5 @@ def solution( n, grid ):
 def prod(i,j,di,dj,n,grid):
  p = 1 
  for k in range(n):
-  p *= grid[j+k*dj][i+k*di]
+  p *= grid[i+k*di][j+k*dj]
  return p 
