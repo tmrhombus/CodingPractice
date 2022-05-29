8bacdc03680d8c3e11982d8ea4dcdcb34dc3e985
Tom Perry <tomperry7@gmail.com>
2022-05-29 09:20:35 -0600

    method for solution, skeleton

---
+++ projecteuler/prob11/solution.py
@@ -0,0 +1,18 @@
+import math 
+
+def solution( n, grid ):
+ """
+ returns the largest product of n sequential numbers
+ where sequential can mean horizontal, vertical, diagonal
+
+ method:
+ work top to bottom, left to right
+ at grid point (i,j) only check right,down,downward diagonals
+ """
+
+ index = [-1,-1]
+ nums = [-1,-1,-1,5]
+
+ return index, nums
+
+ 
