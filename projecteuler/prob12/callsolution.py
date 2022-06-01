235a6df4ade43c7e75041719051e10827e68a073
Tom Perry <tomperry7@gmail.com>
2022-05-31 20:36:48 -0600

    framework for in/outputs

---
+++ projecteuler/prob12/callsolution.py
@@ -0,0 +1,18 @@
+import solution as sol
+
+# nd = minimum number of divisors solution must have
+ndivreq = 4
+
+lastdigit,solnum,primefactors,divisors   = sol.solution(  )
+  
+#print("\n\nThe sum 1+2+...+{} = {}, and has {} (> {})  divisors".format(lastdigit,solnum,len(divisors),ndivreq))
+#
+#
+#print("\nThe prime factors are:")
+#for pf in primefactors:
+# print("  {}".format(pf))
+#
+#
+#print("\nThe divisors are:")
+#for div in divisors:
+# print("  {}".format(div))
