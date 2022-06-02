671110ed5f00d138104019d60f1a3f7c45efd5e2
Tom Perry <tomperry7@gmail.com>
2022-06-01 21:13:02 -0600

    turn off printout

--- projecteuler/prob12/solution.py
+++ projecteuler/prob12/solution.py
@@ -30,25 +30,107 @@ def solution( ndivreq ):
 
  """
 
- doprint = True
+ doprint = False
 
- lastdigit = 3
+ lastdigit = 2
  solnum = 3 #triangle number 
- primefactors = []
- divisors = []
  
- solfound = False
-
  while True:
 
-  if (solfound):
+  if(doprint):
+   print("\nlast digit: {}, solnum: {}".format(lastdigit,solnum))
+
+  primefactors = pfactorize( solnum )
+  
+  if(doprint):
+   print(" prime factors: {}".format(primefactors))
+
+  ndiv = 1
+  for prime in primefactors:
+   ndiv *= ( primefactors[prime] + 1 )
+
+  if(doprint):
+   print("  number divisors: {}".format(ndiv))
+
+  if(ndiv>ndivreq):
    break
 
-  print("solnum = {}".format(solnum))
-
-
+  lastdigit += 1
   solnum += lastdigit
 
+ return lastdigit,solnum,primefactors,ndiv
 
- return lastdigit,solnum,primefactors,divisors
+
+def pfactorize( target ):
+ """ 
+ returns all prime factors of target
+ in a hashset
+
+ method:
+
+ find A = lowest prime factor of target by checking 2,3,4,...
+ split target into t = A*B 
+  A is prime, add it to list of primes
+  B becomes new target
+   repeat iteraticely
+
+  if we can not find any factors for target, we're done
+ 
+ """
+
+ doprint = False
+
+ pfactors = {}
+
+ while True:
+  if(doprint):
+   print("finding factor of {}".format(target))
+  pfac = findminfactor( target )
+
+  if(doprint):
+   print("factor is: {}".format(pfac))
+
+  if pfac in pfactors: 
+   pfactors[pfac] += 1
+  else :
+   pfactors.update({pfac:1})
+
+  if(doprint):
+   print("list of pfactors: ")
+   #pfactors.append(int(pfac))
+   print(pfactors)
+   print("\n")
+  
+  if pfac != target:
+   target = target/pfac
+  else:
+    break
+
+ 
+ if(doprint):
+  print("Done!")
+  print("List of factors: ")
+  print(pfactors)
+  print("\n")
+
+ return pfactors
+
+
+def findminfactor( x ):
+ """
+ find first factor of x
+ """
+
+ i=2
+ while i<x:
+  if x%i == 0:
+   return int(i)
+  else:
+   if i==2:
+    i = i+1
+   else:
+    i = i+2
+
+ return int(x)
+
 
