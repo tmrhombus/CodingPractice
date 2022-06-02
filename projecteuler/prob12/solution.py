2d350c6cfe9d25fc092ecbe69b36b5c13e9ef54b
Tom Perry <tomperry7@gmail.com>
2022-06-01 19:24:11 -0600

    method for counting number of factors given prime factorization

--- projecteuler/prob12/solution.py
+++ projecteuler/prob12/solution.py
@@ -6,10 +6,10 @@ def solution( ndivreq ):
  finds the smallest tn which has more
  than <ndivreq> divisors
 
- method:
- check each next triangle number individually
- do this by finding prime factorization of tn
- the total number of primes (including duplicates)
+ Overall Method:
+ Check each next triangle number individually
+ Do this by finding prime factorization of tn
+ The total number of primes (including duplicates)
   multiplies to tn, so every product of primes
   on the list is necessarily smaller than tn
  The each product of ps gives a unique number
@@ -19,6 +19,14 @@ def solution( ndivreq ):
   the total number of combinations of ps
  Check this against <ndivreq> 
 
+ Counting number of factors:
+ Each unique prime factor can appear in the 
+  overall product with 0,1,...,m copies
+  where m is the multiplicity of that p-factor
+ This gives m+1 options for each prime
+ Total number of unique factors will be 
+  Product(m+1) for each m in the list
+ 
 
  """
 
@@ -38,7 +46,6 @@ def solution( ndivreq ):
 
   print("solnum = {}".format(solnum))
 
-  if (
 
   solnum += lastdigit
 
