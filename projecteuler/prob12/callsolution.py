20bb9ff6c51e93d513dd17f629c8d413451e42ef
Tom Perry <tomperry7@gmail.com>
2022-06-01 21:08:03 -0600

    add code to count for ndivisors

--- projecteuler/prob12/callsolution.py
+++ projecteuler/prob12/callsolution.py
@@ -3,16 +3,12 @@ import solution as sol
 # nd = minimum number of divisors solution must have
 ndivreq = 4
 
-lastdigit,solnum,primefactors,divisors   = sol.solution(  )
+lastdigit,solnum,primefactors,ndiv   = sol.solution( ndivreq )
   
-#print("\n\nThe sum 1+2+...+{} = {}, and has {} (> {})  divisors".format(lastdigit,solnum,len(divisors),ndivreq))
-#
-#
-#print("\nThe prime factors are:")
-#for pf in primefactors:
-# print("  {}".format(pf))
-#
-#
-#print("\nThe divisors are:")
-#for div in divisors:
-# print("  {}".format(div))
+print("\n\nThe sum 1+2+...+{} = {}, and has {} (> {})  divisors".format(lastdigit,solnum,ndiv,ndivreq))
+
+
+print("\nThe prime factors are:")
+for pf in primefactors:
+ print("  {}^{}".format(pf,primefactors[pf]))
+
