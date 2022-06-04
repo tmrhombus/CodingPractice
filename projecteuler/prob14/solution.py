3d292203969597eba0aa310242c359e2afce3350
Tom Perry <tomperry7@gmail.com>
2022-06-04 13:55:45 -0600

    collatz chain

--- projecteuler/prob14/solution.py
+++ projecteuler/prob14/solution.py
@@ -6,9 +6,32 @@ def solution( maxn ):
  method: check them all, save the longest
  """
 
+ maxchain = [1,2,3,4]
+
+ doprint=False
+
+ for i in range(1,maxn):
+
+  thischain = makechain(i)
+  
+  if len(thischain)>len(maxchain):
+   maxchain = thischain
+
+ return maxchain
 
 
+def makechain(x):
+ """
+ if x is odd,   -> 3x+1
+ if x is even,  -> x/2
+ """
+ thechain = []
 
- return maxchain:
+ while True:
+  
+  if x==1:
+   return thechain
+
+
 
  
