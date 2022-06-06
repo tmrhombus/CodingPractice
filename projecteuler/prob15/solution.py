c6c777bc5ed07b5f7f416aa47c5de59d17870a86
Tom Perry <tomperry7@gmail.com>
2022-06-05 20:58:33 -0600

    binomial theorem

---
+++ projecteuler/prob15/solution.py
@@ -0,0 +1,16 @@
+
+def solution( vsteps, hsteps ):
+ """
+ returns the total number of possible routes
+ on a grid of dimensions <vsteps> x <hsteps>
+
+ method: 
+ This is the total number of combinations of
+ (identical) v and h steps.
+ There are <vsteps>+<hsteps> choose <vsteps>
+ such combinations.
+ """
+
+
+
+ return nroutes
