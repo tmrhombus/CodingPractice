7ab026a4f1a1774cc715ddc6ff2714c9ca2c5cb5
Tom Perry <tomperry7@gmail.com>
2022-06-06 19:07:11 -0600

    correct solution

--- projecteuler/prob15/callsolution.py
+++ projecteuler/prob15/callsolution.py
@@ -1,8 +1,10 @@
 import solution as sol
 
-vsteps = 2
-hsteps = 2
+vsteps = 20
+hsteps = 20
 
 nroutes = sol.solution( vsteps, hsteps )
 
 print("The total number of routes for a {} by {} grid is {}".format(vsteps,hsteps,nroutes))
+
+
