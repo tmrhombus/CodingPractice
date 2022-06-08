a55054e16108e0d13d3d883d0e679d6a2ae13294
Tom Perry <tomperry7@gmail.com>
2022-06-07 22:01:14 -0600

    working solution checks out

--- projecteuler/prob16/solution.py
+++ projecteuler/prob16/solution.py
@@ -8,7 +8,16 @@ def solution( base, exp ):
  do the calculation directly
  """
 
- 
+ prod = base ** exp
+
+ thesum = sumdigits(prod)
 
  return prod, thesum
 
+
+def sumdigits(n):
+ s = 0
+ while n:
+  s += n % 10
+  n //= 10
+ return s
