539212aa428c9f13e5c928a766857b9c3500beab
Tom Perry <tomperry7@gmail.com>
2022-06-07 21:48:54 -0600

    method

---
+++ projecteuler/prob16/solution.py
@@ -0,0 +1,14 @@
+
+def solution( base, exp ):
+ """
+ returns the numerical value of <base>^<exp> 
+ as well as the sum of the digits
+
+ method:
+ do the calculation directly
+ """
+
+ 
+
+ return prod, thesum
+
