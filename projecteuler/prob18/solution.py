9751e90a2fa58f813618a3fdc81db9e5f4cdf43a
Tom Perry <tomperry7@gmail.com>
2022-06-10 12:43:56 -0600

    method defined

---
+++ projecteuler/prob18/solution.py
@@ -0,0 +1,14 @@
+
+def solution( triangle ):
+ """
+ returns the maximum sum following a path through <triangle>
+
+ method:
+ dynamic programming
+ start at bottom
+ for each row above, add larger of two adjacent options
+ """
+
+
+
+ return thesum
