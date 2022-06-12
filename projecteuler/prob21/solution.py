7ba22502f5df853e8f2dfbacd48abbcec9dd39df
Tom Perry <tomperry7@gmail.com>
2022-06-12 10:15:51 -0600

    pfactorize function

--- projecteuler/prob21/solution.py
+++ projecteuler/prob21/solution.py
@@ -13,3 +13,81 @@ def solution( topnum ):
   thesum += an
 
  return ans, thesum
+
+
+
+
+def pfactorize( target ):
+ """
+ returns all prime factors of target
+ in a hashset
+
+ method:
+
+ find A = lowest prime factor of target by checking 2,3,4,...
+ split target into t = A*B 
+  A is prime, add it to list of primes
+  B becomes new target
+   repeat iteraticely
+
+  if we can not find any factors for target, we're done
+ 
+ """
+
+ doprint = False
+
+ pfactors = {}
+
+ while True:
+  if(doprint):
+   print("finding factor of {}".format(target))
+  pfac = findminfactor( target )
+
+  if(doprint):
+   print("factor is: {}".format(pfac))
+
+  if pfac in pfactors:
+   pfactors[pfac] += 1
+  else :
+   pfactors.update({pfac:1})
+
+  if(doprint):
+   print("list of pfactors: ")
+   #pfactors.append(int(pfac))
+   print(pfactors)
+   print("\n")
+
+  if pfac != target:
+   target = target/pfac
+  else:
+    break
+
+ if(doprint):
+  print("Done!")
+  print("List of factors: ")
+  print(pfactors)
+  print("\n")
+
+ return pfactors
+
+
+def findminfactor( x ):
+ """
+ find first factor of x
+ """
+
+ i=2
+ while i<x:
+  if x%i == 0:
+   return int(i)
+  else:
+   if i==2:
+    i = i+1
+   else:
+    i = i+2
+
+ return int(x)
+
+
+
+
