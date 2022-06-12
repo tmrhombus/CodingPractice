840c1eeef20aee47b514c39373e64ebdf12f3c4d
Tom Perry <tomperry7@gmail.com>
2022-06-12 10:11:29 -0600

    problem setup

---
+++ projecteuler/prob21/callsolution.py
@@ -0,0 +1,8 @@
+
+import solution as sol
+
+topnum = 100
+
+ans, thesum = sol.solution( topnum )
+
+
---
+++ projecteuler/prob21/solution.py
@@ -0,0 +1,15 @@
+
+def solution( topnum ):
+ """
+ returns the set of amicable numbers less than <topnum>
+ as well as their sum
+
+ """
+
+ ans = []
+
+ thesum = 0
+ for an in ans:
+  thesum += an
+
+ return ans, thesum
