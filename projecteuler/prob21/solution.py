4d1f7455723e8cb21508247ab53ac62cda59e5b8
Tom Perry <tomperry7@gmail.com>
2022-06-14 20:42:06 -0600

    implement check for ANs

--- projecteuler/prob21/solution.py
+++ projecteuler/prob21/solution.py
@@ -17,11 +17,22 @@ def solution( topnum ):
   # for each i, increment j as multiples of i
   # up to <topnum>. Don't include i itself
   # because i is not a "proper" divisor of i 
-  for j in range(i * 2, len(divisorsum), i):
+  for j in range(i * 2, topnum, i):
    # j is a multiple of i, so i is a divisor of j
    # add i to the divisor sum at index j
    divisorsum[j] += i
 
- print("divisor sums: ")
- for k in range(topnum):
-  print(" k={}, sum={}".format(k,divisorsum[k]))
+ #print("divisor sums: ")
+ #for k in range(topnum):
+  #print(" k={}, sum={}".format(k,divisorsum[k]))
+
+ # Find all amicable pairs within range
+ ans = []
+ thesum = 0
+ for i in range(1, topnum):
+  j = divisorsum[i]
+  if j != i and j < topnum and divisorsum[j] == i:
+   thesum += i
+   ans.append([i,j])
+
+ return ans, thesum
