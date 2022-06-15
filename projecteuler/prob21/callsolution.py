5be7efd4ba0177eccb10712dfb5b74c3a91bf920
Tom Perry <tomperry7@gmail.com>
2022-06-14 20:42:24 -0600

    correct solution

--- projecteuler/prob21/callsolution.py
+++ projecteuler/prob21/callsolution.py
@@ -1,8 +1,10 @@
 
 import solution as sol
 
-topnum = 100
+topnum = 10000
 
 ans, thesum = sol.solution( topnum )
 
-
+print("The amicable numbers from 1 to {} are".format(topnum))
+print(ans)
+print("They sum to {}".format(thesum))
