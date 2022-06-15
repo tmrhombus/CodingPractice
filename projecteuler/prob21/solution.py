1519e2deed3776ea0f3f1d04cc30e86df69cea02
Tom Perry <tomperry7@gmail.com>
2022-06-14 20:34:24 -0600

    compute divisor sum directly

--- projecteuler/prob21/solution.py
+++ projecteuler/prob21/solution.py
@@ -4,125 +4,24 @@ def solution( topnum ):
  returns the set of amicable numbers less than <topnum>
  as well as their sum
 
- """
-
- i=60
-# for i in range(2,topnum+1):
-#  pfacs = pfactorize(i)
-#  print(" i={}, facs={}".format(i,pfacs))
-
- pfacs = pfactorize(i)
- print(" i={}, facs={}".format(i,pfacs))
- 
- prelist = []
- for j in pfacs:
-  #print("  j: {}".format(j))
-  for k in range(1,pfacs.get(j)+1):
-   #fac = j**k
-   #print("   k={}, pfacs[{}]={},  fac={}".format( k, j, pfacs[j], fac ))
-   prelist.append(j**k)
-
- print("prelist = {}".format(prelist))
-
- 
- thecombs = combos(prelist)
- print("thecombs = {}".format(thecombs))
-
- faclist = []
- for j in thecombs:
-  prod = 1
-  for k in j:
-   prod *= k
-  faclist.append(prod)
-
- print(faclist)
-
- ans = []
-
- thesum = 0
- for an in ans:
-  thesum += an
-
- return ans, thesum
-
-
-
-
-def pfactorize( target ):
- """
- returns all prime factors of target in a hashset
-
  method:
- find A = lowest prime factor of target by checking 2,3,4,...
- split target into t = A*B 
-  A is prime, add it to list of primes
-  B becomes new target
-   repeat iteraticely
-  if we can not find any factors for target, we're done
+ make an array of the sums of divisors
+ search that array for amicable pairs
  """
 
- doprint = False
+ # Compute sum of proper divisors for each number
+ # make array of zeros of length <topnum>
+ divisorsum = [0] * topnum
+ # loop through i = 1 to <topnum>
+ for i in range(1, topnum):
+  # for each i, increment j as multiples of i
+  # up to <topnum>. Don't include i itself
+  # because i is not a "proper" divisor of i 
+  for j in range(i * 2, len(divisorsum), i):
+   # j is a multiple of i, so i is a divisor of j
+   # add i to the divisor sum at index j
+   divisorsum[j] += i
 
- pfactors = {}
-
- while True:
-  if(doprint):
-   print("finding factor of {}".format(target))
-  pfac = findminfactor( target )
-
-  if(doprint):
-   print("factor is: {}".format(pfac))
-
-  if pfac in pfactors:
-   pfactors[pfac] += 1
-  else :
-   pfactors.update({pfac:1})
-
-  if(doprint):
-   print("list of pfactors: ")
-   #pfactors.append(int(pfac))
-   print(pfactors)
-   print("\n")
-
-  if pfac != target:
-   target = target/pfac
-  else:
-    break
-
- if(doprint):
-  print("Done!")
-  print("List of factors: ")
-  print(pfactors)
-  print("\n")
-
- return pfactors
-
-
-def findminfactor( x ):
- """
- find first factor of x
- """
-
- i=2
- while i<x:
-  if x%i == 0:
-   return int(i)
-  else:
-   if i==2:
-    i = i+1
-   else:
-    i = i+2
-
- return int(x)
-
-
-def combos(a):
- """
- all return all combinations of elements in list a
- """
- if len(a) == 0:
-  return [[]]
- cs = []
- for c in combos(a[1:]):
-  cs += [c, c+[a[0]]]
- return cs
+ print("divisor sums: ")
+ for k in range(topnum):
+  print(" k={}, sum={}".format(k,divisorsum[k]))
