d8f30b521a39037197722321962598c77d8223ef
Tom Perry <tomperry7@gmail.com>
2022-06-20 21:16:43 -0600

    sum of divisors

--- projecteuler/prob23/solution.py
+++ projecteuler/prob23/solution.py
@@ -1,8 +1,33 @@
 
-def solution(themax):
+def solution(topnum):
  """
- returns the sum of all integers which can not be
- expressed as the sum of two abundant numbers
- less than <themax>
+ returns the sum of all integers less
+ than <topnum>  which can not be expressed
+ as the sum of two abundant numbers
+
+ method:
+ 1. make an array of the sum of divisors as in prob21
+    divisorsum[j] corresponds to sum of divisors of j
  """
+ 
+ # Compute sum of proper divisors for each number
+ # make array of zeros of length <topnum>
+ divisorsum = [0] * topnum
+ # loop through i = 1 to <topnum>
+ for i in range(1, topnum):
+  # for each i, increment j as multiples of i
+  # up to <topnum>. Don't include i itself
+  # because i is not a "proper" divisor of i 
+  for j in range(i * 2, topnum, i): 
+   # j is a multiple of i, so i is a divisor of j
+   # add i to the divisor sum at index j
+   divisorsum[j] += i
+
+
+
+
+
+ thesum = 0
+
+ return thesum
 
