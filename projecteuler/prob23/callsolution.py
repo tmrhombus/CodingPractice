0d474d33228a7908b25383383cb4e6af2161f576
Tom Perry <tomperry7@gmail.com>
2022-06-20 21:00:29 -0600

    initial setup

---
+++ projecteuler/prob23/callsolution.py
@@ -0,0 +1,8 @@
+
+import solution as sol
+
+themax = 100
+
+thesum = sol.solution()
+
+print("The sum of all integers under {} which can not be written as the sum of two abundant numbers is {}".format(themax, thesum))
