81e365b674f226a90e42a37dd045e6c04654082e
Tom Perry <tomperry7@gmail.com>
2022-06-22 13:20:44 -0600

    list abundant numbers

--- projecteuler/prob23/callsolution.py
+++ projecteuler/prob23/callsolution.py
@@ -3,7 +3,8 @@ import solution as sol
 
 topnum = 100
 
-thesum,nonexpressable = sol.solution(topnum)
+thesum,abundants,nonexpressable = sol.solution(topnum)
 
 print("The sum of all integers under {} which can not be written as the sum of two abundant numbers is {}".format(topnum, thesum))
 print("The list of nonexpressable numbers is:\n {}".format(nonexpressable))
+print("The list of abundant numbers is:\n {}".format(abundants))
