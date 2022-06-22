8bfea2a17ccff04e6eb2f791d504f2d7fb3d20f0
Tom Perry <tomperry7@gmail.com>
2022-06-22 13:26:51 -0600

    don't pring full lists

--- projecteuler/prob23/callsolution.py
+++ projecteuler/prob23/callsolution.py
@@ -1,10 +1,10 @@
 
 import solution as sol
 
-topnum = 100
+topnum = 28123
 
 thesum,abundants,nonexpressable = sol.solution(topnum)
 
 print("The sum of all integers under {} which can not be written as the sum of two abundant numbers is {}".format(topnum, thesum))
-print("The list of nonexpressable numbers is:\n {}".format(nonexpressable))
-print("The list of abundant numbers is:\n {}".format(abundants))
+#print("The list of nonexpressable numbers < {} is:\n {}".format(topnum,nonexpressable))
+#print("The list of abundant numbers < {} is:\n {}".format(topnum,abundants))
