0a0616a627098423c1d9422a4caa48ee937d57c3
Tom Perry <tomperry7@gmail.com>
2022-06-22 13:18:46 -0600

    include list of NE numbers

--- projecteuler/prob23/callsolution.py
+++ projecteuler/prob23/callsolution.py
@@ -3,6 +3,7 @@ import solution as sol
 
 topnum = 100
 
-thesum = sol.solution(topnum)
+thesum,nonexpressable = sol.solution(topnum)
 
 print("The sum of all integers under {} which can not be written as the sum of two abundant numbers is {}".format(topnum, thesum))
+print("The list of nonexpressable numbers is:\n {}".format(nonexpressable))
