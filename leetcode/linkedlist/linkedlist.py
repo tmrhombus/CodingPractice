400de791155efc2ce45fa71d59502149a28bcbcd
Tom Perry <tomperry7@gmail.com>
2022-06-25 15:39:03 -0600

    printlist, start addathead

--- leetcode/linkedlist/linkedlist.py
+++ leetcode/linkedlist/linkedlist.py
@@ -1,23 +1,58 @@
 
+
+# Node class
+class Node:
+  
+ # Function to initialize the node object
+ def __init__(self, data):
+  self.data = data  # Assign data
+  self.next = None  # Initialize
+                       # next as null
+  
 class MyLinkedList:
 
-    def __init__(self):
+ def __init__(self):
+  self.head = None
+     
+ def printList(self):
+  temp = self.head
+  while (temp):
+   print (temp.data)
+   temp = temp.next
+
+# def get(self, index: int) -> int:
+#     
+#
+ def addAtHead(self, val: int) -> None:
+  new_node = Node(val)
+     
+
+# def addAtTail(self, val: int) -> None:
+#     
+#
+# def addAtIndex(self, index: int, val: int) -> None:
+#     
+#
+# def deleteAtIndex(self, index: int) -> None:
         
 
-    def get(self, index: int) -> int:
-        
 
-    def addAtHead(self, val: int) -> None:
-        
+ 
+# Code execution starts here
+if __name__=='__main__':
+ 
+    # Start with the empty list
+    llist = MyLinkedList()
+ 
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+ 
+    llist.head.next = second; # Link first node with second
+    second.next = third; # Link second node with the third node
+ 
+    llist.printList()
 
-    def addAtTail(self, val: int) -> None:
-        
-
-    def addAtIndex(self, index: int, val: int) -> None:
-        
-
-    def deleteAtIndex(self, index: int) -> None:
-        
 
 
 # Your MyLinkedList object will be instantiated and called as such:
