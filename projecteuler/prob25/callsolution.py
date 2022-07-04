faf51ae5d318df03444473053068ca8d25f601e3
Tom Perry <tomperry7@gmail.com>
2022-07-04 17:59:25 -0600

    solution setup

---
+++ projecteuler/prob25/callsolution.py
@@ -0,0 +1,9 @@
+
+import solution as sol
+
+ndigits = 5
+
+thenum, index = sol.solution(ndigits)
+
+print("The index of the Fibonocci number with {} digits is {}".format(ndigits,index))
+print("The number itself is {}".format(thenum))
