2fc5d0ff6ca54e0762800a92deffb347ac6daa84
Tom Perry <tomperry7@gmail.com>
2024-08-23 22:31:06 -0600

    run linked list

--- leetcode/linkedlist/runll.py
+++ leetcode/linkedlist/runll.py
@@ -3,11 +3,16 @@ import linkedlist as ll
 
 llist = ll.MyLinkedList()
 
-llist.addAtIndex(1,0)
-llist.get(0)
 llist.addAtHead(4)
 llist.addAtHead(3)
 llist.addAtHead(2)
 llist.addAtHead(1)
-for i in range(llist.count):
- print("i={}, data={}".format(i,llist.get(i)))
+llist.addAtTail(9)
+
+print("original list:")
+llist.printList()
+
+print("------reverse-----")
+llist.reverseList()
+
+llist.printList()
