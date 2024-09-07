7390c6f10ad0958b99216f4e253a7998af2a8bad
Tom Perry <tomperry7@gmail.com>
2024-09-07 14:53:06 -0600

    contains duplicate solution

--- leetcode/12_771_JewelsStones/python/runsol.py
+++ leetcode/12_771_JewelsStones/python/runsol.py
@@ -9,4 +9,3 @@ out = sol.numJewelsInStones(jewels, stones)
 print(f"Jewels: {jewels}")
 print(f"Stones: {stones}")
 print(f"Count: {out}")
-
---
+++ leetcode/13_217_ContainsDuplicate/python/runsol.py
@@ -0,0 +1,11 @@
+from solution import Solution 
+
+nums = [1,1,1,3,3,4,3,2,4,2]
+
+#nums = [1,2,3,4]
+
+sol = Solution()
+out = sol.containsDuplicate(nums)
+ 
+print(f"Nums:  {nums}")
+print(f"Dupes: {out}")
