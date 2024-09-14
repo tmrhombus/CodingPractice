ba3306f958bdd4bb22ed6e4f1711242bca06d240
Tom Perry <tomperry7@gmail.com>
2024-09-13 23:43:07 -0600

    projecteuler prob22

---
+++ projecteuler/prob22/sol1.py
@@ -0,0 +1,16 @@
+import csv
+
+def solution(infilename):
+ """
+ takes an input list of names
+ sorts the names alphabetically
+ computes a letter value for each name
+  by summing all letters as a=1, b=2, ...
+ computes name score as (letter value)*position
+ sums all of the name scores and returns the sum
+ """
+
+
+
+
+define method
