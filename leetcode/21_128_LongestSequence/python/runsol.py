a26c0e6bb687a5b00a78e2947f56aff190c59876
Tom Perry <tomperry7@gmail.com>
2024-09-18 23:25:10 -0600

    longest sequence

---
+++ leetcode/21_128_LongestSequence/python/runsol.py
@@ -0,0 +1,8 @@
+from solution import Solution 
+
+nums = [100,4,200,1,3,2]
+
+sol = Solution()
+out = sol.longestConsecutive(nums)
+
+print(f"{out}")
---
+++ leetcode/21_128_LongestSequence/python/solution.py
@@ -0,0 +1,12 @@
+from typing import List
+from collections import Counter
+import collections
+
+class Solution:
+ def longestConsecutive(self, nums: List[int]) -> int:
+  dict_seqs = {}
+
+  for i in nums:
+   print(i)
+
+  return 1
