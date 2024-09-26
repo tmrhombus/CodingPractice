2cf05fbbedd767a1533986310e2acdbc0531e373
Tom Perry <tomperry7@gmail.com>
2024-09-25 22:08:05 -0600

    faster solution

--- leetcode/22_977_SquaredOfSortedArr/python/solution.py
+++ leetcode/22_977_SquaredOfSortedArr/python/solution.py
@@ -9,21 +9,34 @@ class Solution:
 
   s = len(nums)
   out = s*[0]
-  R = s-1
-  L = 0
 
   for i in range(s):
-   print(f"R: {R}, L: {L}")
-   val_r = nums[R]**2
-   val_l = nums[L]**2
-   if val_r > val_l:
-    out[s-i-1] = val_r
-    R -= 1
-   else:
-    out[s-i-1] = val_l
-    L += 1
+   out[i] = nums[i]**2
 
-  return out
+  return sorted(out)
+
+
+# def sortedSquares(self, nums: List[int]) -> List[int]:
+#
+#  if len(nums)==1: return nums[0]**2
+#
+#  s = len(nums)
+#  out = s*[0]
+#  R = s-1
+#  L = 0
+#
+#  for i in range(s):
+#   print(f"R: {R}, L: {L}")
+#   val_r = nums[R]**2
+#   val_l = nums[L]**2
+#   if val_r > val_l:
+#    out[s-i-1] = val_r
+#    R -= 1
+#   else:
+#    out[s-i-1] = val_l
+#    L += 1
+#
+#  return out
 
    
     
