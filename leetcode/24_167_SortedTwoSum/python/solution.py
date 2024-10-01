53c0e0fdbb958b7c18580e763a56d62b88559bc5
Tom Perry <tomperry7@gmail.com>
2024-09-30 20:43:20 -0600

    faster solution sorted two sum

--- leetcode/24_167_SortedTwoSum/python/solution.py
+++ leetcode/24_167_SortedTwoSum/python/solution.py
@@ -2,19 +2,59 @@ from typing import List
 from collections import Counter
 import collections
 
+
 class Solution:
  def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+  if len(numbers)==2:
+   return [1,2]
 
   L = 0
   R = len(numbers)-1
 
   while True: 
    thesum = numbers[L] + numbers[R]
-   if thesum == target:
-    return [L+1,R+1]
-
-   elif thesum > target:
+   if thesum > target:
     R -= 1
+    continue
    elif thesum < target:
     L += 1
+    continue
+   return [L+1,R+1]
+
+#class Solution:
+# def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#
+#  if len(numbers)==2:
+#   return [1,2]
+#
+#  L = 0
+#  R = len(numbers)-1
+#
+#  while True: 
+#   thesum = numbers[L] + numbers[R]
+#   if thesum == target:
+#    return [L+1,R+1]
+#
+#   elif thesum > target:
+#    R -= 1
+#   elif thesum < target:
+#    L += 1
+
+
+#class Solution:
+# def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#
+#  L = 0
+#  R = len(numbers)-1
+#
+#  while True: 
+#   thesum = numbers[L] + numbers[R]
+#   if thesum == target:
+#    return [L+1,R+1]
+#
+#   elif thesum > target:
+#    R -= 1
+#   elif thesum < target:
+#    L += 1
 
