5fe029db211551e4e7bdc8a2e7fd3359632a5712
Tom Perry <tomperry7@gmail.com>
2025-08-08 23:17:14 -0600

    working solution

--- stratascratch/10401_AppleProductCounts/solution.py
+++ stratascratch/10401_AppleProductCounts/solution.py
@@ -1,48 +1,48 @@
-
 # Import your libraries
 import pyspark
 import pyspark.sql.functions as F
 
 # Start writing code
 
-valid_devices = [
+mac_devices = [
     "macbook pro",
     "iphone 5s",
     "ipad air"
 ]
 
-df_valid_users = (
+df_all_users = (
     playbook_events
-    .where(F.col("device").isin(valid_devices))
     .select("user_id")
     .distinct()
 )
 
-df_user_language_counts = (
-    df_valid_users
+df_mac_users = (
+    playbook_events
+    .where(F.col("device").isin(mac_devices))
+    .select("user_id")
+    .distinct()
+)
+
+df_mac_language_counts = (
+    df_mac_users
     .join(playbook_users, on="user_id", how="left")
-    .select("user_id", "language")
     .groupBy("language")
-    .agg(
-        F.count("user_id").alias("nr_valid_users")
-    )
+    .agg(F.count("user_id").alias("nr_mac_users"))
 )
 
 df_all_language_counts = (
-    playbook_users
+    df_all_users
+    .join(playbook_users, on="user_id", how="left")
     .groupBy("language")
-    .agg(
-        F.count("user_id").alias("nr_total_users")    
-    )
+    .agg(F.count("user_id").alias("nr_all_users"))
 )
 
 df_out = (
-    df_user_language_counts
-    .join(df_all_language_counts, on="language", how="right")
+    df_all_language_counts
+    .join(df_mac_language_counts, on="language", how="left")
     .fillna(0)
-    .select("language", "nr_valid_users", "nr_total_users")
+    .select("language", "nr_mac_users", "nr_all_users")
 )
 
-
 # To validate your solution, convert your final pySpark df to a pandas df
-df_out.orderBy(F.col("nr_total_users").desc()).toPandas()
+df_out.orderBy(F.col("nr_all_users").desc()).toPandas()
