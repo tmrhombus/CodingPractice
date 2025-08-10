3acee1388a873c741ac096352b41db6aac04ec52
Tom Perry <tomperry7@gmail.com>
2025-08-09 22:35:09 -0600

    working solution

---
+++ stratascratch/10090_PercentShippableOrders/solution.py
@@ -0,0 +1,40 @@
+
+# Import your libraries
+import pyspark
+import pyspark.sql.functions as F
+
+df_customer_status = (
+    customers
+    .withColumn("isShippable",
+        F.when(
+            ((F.col("address").isNotNull()) & (F.col("city").isNotNull())), 
+        1).otherwise(0)
+    )
+    .select(
+        F.col("id").alias("cust_id"),
+        F.col("isShippable")
+    )
+)
+
+df_valid_order_pct = (
+    orders
+    .join(df_customer_status, on="cust_id", how="left")
+    .fillna(0)
+    .agg(
+        F.sum(
+         F.when(
+          F.col("isShippable")==1
+         ,1).otherwise(0)
+        ).alias("nr_shippable_orders"),
+        F.count("id").alias("nr_all_orders")
+    )
+    .withColumn("pct_shippable_orders",
+        100 * F.col("nr_shippable_orders") / F.col("nr_all_orders")
+        
+    )
+    .select("pct_shippable_orders")
+)
+
+df_valid_order_pct.toPandas()
+
+
